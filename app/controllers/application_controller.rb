48c636d0e0e77d5ad85dd96872b4a4d0b3351614
ivangarrera <iesivangarcia@gmail.com>
2017-01-28 16:47:37 +0100

    Create sessions to log in and log out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,24 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    #If current_user isn't empty, then don't consult the DB. On this way, we avoid multiple consults into the DB
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    # !! convert anything to a boolean.
+    !!current_user
+  end
+
+  def require_user
+    if !logged_in?
+      flash[:danger] = "You must be logged in to perform that action"
+      redirect_to root_path
+    end
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-
+  before_action :set_user, only: [:edit, :update, :show]
   def new
     @user = User.new
   end
@@ -19,12 +19,10 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
 
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = "Your account was updated successfully"
       redirect_to articles_path
@@ -34,13 +32,16 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
     @user_articles = @user.article.paginate(page: params[:page],per_page: 5)
   end
 
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 
 end
