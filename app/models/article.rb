29231a65892316254f702b01939cb21989ffdbab
ivangarrera <iesivangarcia@gmail.com>
2017-01-28 11:39:24 +0100

    add association between user and articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,9 @@
 class Article < ActiveRecord::Base
+  belongs_to :user
+
   #To ensure that the title's column isn't empty and have a minimum and maximum length
   validates :title, presence: true, length: {minimum: 3, maximum: 50}
   #To ensure that the description's column isn't empty and have a minimum and maximum length
   validates :description, presence:true, length: {minimum:10, maximum:300}
+  validates :user_id, presence:true
 end
